3f7ca54a4a875ed64bb51abe51042f09e140dd6e
Nuroldin U. Pimping <pimping.nu72@s.msumain.edu.ph>
2025-03-03 12:05:02 +0800

    Add sign-in functionality and update avatar placeholder image.

--- constants/index.ts
+++ constants/index.ts
@@ -81,7 +81,6 @@ export const sortTypes = [
 	},
 ];
 
-export const avatarPlaceholderUrl =
-	"https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png";
+export const avatarPlaceholderUrl = "/assets/images/avatar.jpg";
 
 export const MAX_FILE_SIZE = 50 * 1024 * 1024; // 50MB
--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
 		remotePatterns: [
 			{
 				protocol: "https",
-				hostname: "cdn.pixabay.com",
+				hostname: "sm.ign.com",
 			},
 			{
 				protocol: "https",
